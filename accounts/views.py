a7ed71bb4cff9b848c343a4eec0c4654ebd63189
Arber00 <arberbedini2000@gmail.com>
2026-06-09 14:28:29 +0200

    Progressing pas buke

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,32 @@
-from django.http import HttpResponse
+from django.contrib.auth import login
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.views import LoginView
+from django.shortcuts import redirect, render
+from django.shortcuts import render
+
+
 
 def home(request):
-    return HttpResponse("Student AI Platform")
+    return render(request, 'accounts/home.html')
+
+
+class AccountLoginView(LoginView):
+    template_name = 'accounts/login.html'
+    authentication_form = AuthenticationForm
+    redirect_authenticated_user = True
+
+
+login_view = AccountLoginView.as_view()
+
+
+def register_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'accounts/register.html', {'form': form})
